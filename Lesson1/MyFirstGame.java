ed118b71be88f530250e2e8ae498e96b7a513f60
RSergey <ReshetkoSergey@mail.ru>
2019-11-12 06:52:22 +0300

    MyfirstGame remade

--- Lesson1/MyFirstGame.java
+++ Lesson1/MyFirstGame.java
@@ -1,30 +1,26 @@
 public class MyFirstGame {
 
     public static void main(String[] args) {
-        int attempt = 39;           // число которое меняется игроком
-        final int guessedNumber = 30;    // загаданное число
+        int attempt = 45;
+        final int guessedNumber = 55;
 
-        if (attempt >= 0 && attempt <= 100) {
-            System.out.println("You entered the number : " + attempt);
-                if (guessedNumber != attempt) {
-                    if (attempt <= guessedNumber-30) {
-                        System.out.println("your number is much smaller");
-                    } else if (attempt >= guessedNumber+30) {
-                        System.out.println("your number is much larger");
-                    } else if (attempt <= guessedNumber-15) {
-                        System.out.println("your number is smaller");
-                    } else if (attempt >= guessedNumber+15) {
-                        System.out.println("your number is bigger");
-                    } else if (attempt <= guessedNumber-1) {
-                        System.out.println("your number is slightly smaller");
-                    } else if (attempt >= guessedNumber+1) {
-                        System.out.println("your number is slightl largeyr");
+        System.out.println("You entered the number: " + attempt);
+        do {
+            if (attempt != guessedNumber) {
+                if (attempt > 0 && attempt < 100) {
+                    if (attempt < guessedNumber) {
+                        System.out.println("The number entered by you is more (less) than what the computer wished for");
+                    } else if (attempt > guessedNumber) {
+                        System.out.println("The number entered by you is more (more) that the computer made a wish");
                     }
                 } else {
-                    System.out.println("You guessed it! Guessed number : " + guessedNumber);
+                    System.out.println("The number is not in the range from 0 to 100");
                 }
-        } else {
-            System.out.println("The number is not in the range from 0 to 100");
-        }
+                System.out.println("Enter a number: ");
+                break;
+            } else {
+                System.out.println("You guessed it!");
+            }
+        } while (attempt != guessedNumber);
     }
 }
